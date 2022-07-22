bae422eedfeac4a0670d8fa2d727166b4a818362
javiermorales1127@gmail.com <18241001@leon.tecnm.mx>
2022-07-22 14:43:17 -0500

    first commit

---
+++ src/js/api.js
@@ -0,0 +1,16 @@
+const idQuote = document.getElementById("id-advice");
+const text = document.getElementById("quote");
+const dice = document.getElementById("btn");
+
+fetch('https://api.adviceslip.com/advice')
+    .then(response=>response.json())
+    .then(json => generateRandomQuote(json))
+
+dice.addEventListener('click', _ =>  location.reload());
+
+
+function generateRandomQuote(quote){
+    text.innerHTML = quote.slip.advice;
+    idQuote.innerHTML = quote.slip.id;
+}
+
