8c313478703ccb280fdd65ca47130c1ed84bd062
javiermorales1127@gmail.com <18241001@leon.tecnm.mx>
2022-07-22 14:53:50 -0500

    media query fixed

--- src/js/api.js
+++ src/js/api.js
@@ -2,15 +2,13 @@ const idQuote = document.getElementById("id-advice");
 const text = document.getElementById("quote");
 const dice = document.getElementById("btn");
 
-fetch('https://api.adviceslip.com/advice')
-    .then(response=>response.json())
-    .then(json => generateRandomQuote(json))
+fetch("https://api.adviceslip.com/advice")
+  .then((response) => response.json())
+  .then((json) => generateRandomQuote(json));
 
-dice.addEventListener('click', _ =>  location.reload());
+dice.addEventListener("click", (_) => location.reload());
 
-
-function generateRandomQuote(quote){
-    text.innerHTML = quote.slip.advice;
-    idQuote.innerHTML = quote.slip.id;
+function generateRandomQuote(quote) {
+  text.innerHTML = quote.slip.advice;
+  idQuote.innerHTML = quote.slip.id;
 }
-
